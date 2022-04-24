0ec5640ed31d8e3737ce4cd07843f8801617a402
B E N <93186925+BenIzak@users.noreply.github.com>
2022-04-24 22:36:09 +0200

    FINAL code for play - team - home : pages

---
+++ storage/home page/pages/play.js
@@ -0,0 +1,20 @@
+var slider_img = document.querySelector('.slider-img');
+var images = ['../images/1.png', '../images/2.png', '../images/3.png', '../images/4.png', '../images/5.png','../images/6.png','../images/7.png'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "../images/"+images[i]);
+	
+}
