b60b5813181de8b68f8aef21044bf3d09844ed6f
B E N <93186925+BenIzak@users.noreply.github.com>
2022-04-24 23:04:20 +0200

    true final code

--- storage/home page/pages/play.js
+++ storage/home page/pages/play.js
@@ -1,5 +1,5 @@
 var slider_img = document.querySelector('.slider-img');
-var images = ['../images/1.png', '../images/2.png', '../images/3.png', '../images/4.png', '../images/5.png','../images/6.png','../images/7.png'];
+var images = ['../images/1.png', '../images/2.png', '../images/3.png', '../images/4.png', '../images/5.png','../images/6.png','../images/7.png','../images/8.png'];
 var i = 0;
 
 function prev(){
@@ -12,7 +12,7 @@ function next(){
 	if(i >= images.length-1) i = -1;
 	i++;
 	return setImg();			 
-}
+} 
 
 function setImg(){
 	return slider_img.setAttribute('src', "../images/"+images[i]);
